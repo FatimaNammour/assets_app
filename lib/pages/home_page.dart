afaebdde95bdfedfea243b446ae04a89d793c7df
FatimaNamm9 <nammouraftima@gmail.com>
2024-02-17 16:57:58 +0300

    fix scaffold issue

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,12 @@
 import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
+import 'package:assets_app/pages/images_assets.dart';
+import 'package:assets_app/pages/videos_page.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
   State<StatefulWidget> createState() {
     return HomePageState();
@@ -11,55 +15,79 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   late NotchBottomBarController _barController;
+  late PageController _controller;
 
   @override
   void initState() {
+    _controller = PageController(initialPage: 0);
     _barController = NotchBottomBarController(index: 0);
     super.initState();
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  final List<Widget> bottomBarPages = [
+    const PhotoPage(),
+    const VideoPage(),
+    const PhotoPage(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: AnimatedNotchBottomBar(
-        bottomBarItems: [
-          BottomBarItem(
-              activeItem: SvgPicture.asset(
-                  'assets/svg/image-plus-svgrepo-com.svg',
-                  colorFilter:
-                      const ColorFilter.mode(Colors.green, BlendMode.srcIn)),
-              inActiveItem: SvgPicture.asset(
-                  'assets/svg/image-plus-svgrepo-com.svg',
-                  colorFilter:
-                      const ColorFilter.mode(Colors.grey, BlendMode.srcIn)),
-              itemLabel: "photo"),
-          BottomBarItem(
-              activeItem: SvgPicture.asset(
-                  'assets/svg/video-plus-svgrepo-com.svg',
-                  colorFilter:
-                      const ColorFilter.mode(Colors.green, BlendMode.srcIn)),
-              inActiveItem: SvgPicture.asset(
-                  'assets/svg/video-plus-svgrepo-com.svg',
-                  colorFilter:
-                      const ColorFilter.mode(Colors.grey, BlendMode.srcIn)),
-              itemLabel: "video"),
-          BottomBarItem(
-              activeItem: SvgPicture.asset(
-                  'assets/svg/audio-square-svgrepo-com.svg',
-                  colorFilter:
-                      const ColorFilter.mode(Colors.green, BlendMode.srcIn)),
-              inActiveItem: SvgPicture.asset(
-                  'assets/svg/audio-square-svgrepo-com.svg',
-                  colorFilter:
-                      const ColorFilter.mode(Colors.grey, BlendMode.srcIn)),
-              itemLabel: "audio"),
-        ],
-        notchBottomBarController: _barController,
-        onTap: (int value) {},
-        kIconSize: 25,
-        kBottomRadius: 10,
+    return SafeArea(
+      child: Scaffold(
+        extendBody: true,
+        bottomNavigationBar: AnimatedNotchBottomBar(
+          bottomBarItems: [
+            BottomBarItem(
+                activeItem: SvgPicture.asset(
+                    'assets/svg/image-plus-svgrepo-com.svg',
+                    colorFilter:
+                        const ColorFilter.mode(Colors.green, BlendMode.srcIn)),
+                inActiveItem: SvgPicture.asset(
+                    'assets/svg/image-plus-svgrepo-com.svg',
+                    colorFilter:
+                        const ColorFilter.mode(Colors.grey, BlendMode.srcIn)),
+                itemLabel: "photo"),
+            BottomBarItem(
+                activeItem: SvgPicture.asset(
+                    'assets/svg/video-plus-svgrepo-com.svg',
+                    colorFilter:
+                        const ColorFilter.mode(Colors.green, BlendMode.srcIn)),
+                inActiveItem: SvgPicture.asset(
+                    'assets/svg/video-plus-svgrepo-com.svg',
+                    colorFilter:
+                        const ColorFilter.mode(Colors.grey, BlendMode.srcIn)),
+                itemLabel: "video"),
+            BottomBarItem(
+                activeItem: SvgPicture.asset(
+                    'assets/svg/audio-square-svgrepo-com.svg',
+                    colorFilter:
+                        const ColorFilter.mode(Colors.green, BlendMode.srcIn)),
+                inActiveItem: SvgPicture.asset(
+                    'assets/svg/audio-square-svgrepo-com.svg',
+                    colorFilter:
+                        const ColorFilter.mode(Colors.grey, BlendMode.srcIn)),
+                itemLabel: "audio"),
+          ],
+          notchBottomBarController: _barController,
+          onTap: (int value) {
+            _controller.jumpToPage(value);
+          },
+          kIconSize: 25,
+          kBottomRadius: 10,
+        ),
+        body: PageView(
+          controller: _controller,
+          physics: const NeverScrollableScrollPhysics(),
+          children: List.generate(
+              bottomBarPages.length, (index) => bottomBarPages[index]),
+        ),
       ),
-      body: PageView(children: [Container(), Container(), Container()]),
     );
   }
 }
